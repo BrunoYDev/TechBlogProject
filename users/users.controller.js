7105b7c09e7095d9c77c6dac85dae3cbcda7c7ce
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-29 10:41:40 -0300

    Add: Authentication and Login screen

--- users/users.controller.js
+++ users/users.controller.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("./User");
 const bcrypt = require("bcryptjs");
+const adminAuth = require("../middlewares/admin.auth");
 
 router.get("/admin/users", (req, res) => {
   User.findAll().then(users => {
@@ -22,7 +23,7 @@ router.post("/users/create", (req, res) => {
       email: email,
     },
   }).then((user) => {
-    if (user === undefined) {
+    if (!user) {
       let salt = bcrypt.genSaltSync(10);
 
       let hash = bcrypt.hashSync(password, salt);
@@ -38,9 +39,43 @@ router.post("/users/create", (req, res) => {
           res.redirect("/");
         });
     } else {
+      console.log("error")
       res.redirect("/admin/users/create");
     }
   });
+});
+
+router.get("/users/login", (req,res) => {
+  res.render("admin/users/login");
+});
+
+router.post('/users/authenticate', (req,res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  User.findOne({where:{email: email}}).then((user) => {
+    if(user){
+      let correct = bcrypt.compareSync(password, user.password);
+
+      if(correct){
+        req.session.user = {
+          id: user.id,
+          email: user.email
+        }
+
+        res.redirect('/admin/articles');
+      }else{
+        res.redirect('/users/login');
+      }
+    }else{
+      res.redirect('/users/login');
+    }
+  })
+});
+
+router.get('/users/logout',(req,res) => {
+  req.session.user = undefined;
+  res.redirect('/');
 });
 
 module.exports = router;
